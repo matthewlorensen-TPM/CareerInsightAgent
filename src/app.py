02454e682186c35babc76a0f27d84ce60098d7ea
Matthew Lorensen <matthew.lorensen@gmail.com>
2026-07-17 10:46:47 -0600

    Force specific library versions and update import paths

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 # Use these exact, standard paths
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_chroma import Chroma
-from langchain.chains import create_retrieval_chain
+from langchain_classic.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
